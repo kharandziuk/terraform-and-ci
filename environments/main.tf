c805f943490a4243d6563741a77bac7889945922
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-29 19:54:05 +0100

    add destroy staging task fix deploy staging task fix issues with environments

--- environments/main.tf
+++ environments/main.tf
@@ -12,10 +12,12 @@ variable "aws_region" {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-module "env" {
-  source = "../../"
-  stage  = "staging"
+module "dev_env" {
+  source = "../"
+  stage  = "dev"
 }
