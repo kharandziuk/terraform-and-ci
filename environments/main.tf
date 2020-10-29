0b033bf07b4c81d06cc83ef4a509977dd3b561a1
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-29 14:26:58 +0100

    add dev environment

---
+++ environments/main.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {
+  description = "Access key to your AWS account "
+}
+
+variable "aws_secret_key" {
+  description = "Secret key to your AWS account "
+}
+
+variable "aws_region" {
+  default     = "eu-central-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+module "dev_env" {
+  source = "../"
+}
