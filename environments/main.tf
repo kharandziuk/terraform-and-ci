194b21dd683416ee86a1d0c6c61be694578489fd
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-10-29 14:57:03 +0100

    add cleanup to validate

--- environments/main.tf
+++ environments/main.tf
@@ -12,9 +12,7 @@ variable "aws_region" {
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.aws_region
 }
 
 module "dev_env" {
