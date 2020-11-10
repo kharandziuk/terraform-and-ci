b347d7388e5e2c6221446bcdb735b761346197eb
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-11-10 08:29:40 +0100

    rename aws_* -> *

--- environments/main.tf
+++ environments/main.tf
@@ -1,9 +1,9 @@
-variable "aws_access_key" {
-  description = "Access key to your AWS account "
+variable "access_key" {
+  description = "Access key to your AWS account"
 }
 
-variable "aws_secret_key" {
-  description = "Secret key to your AWS account "
+variable "secret_key" {
+  description = "Secret key to your AWS account"
 }
 
 variable "aws_region" {
@@ -13,11 +13,11 @@ variable "aws_region" {
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
-module "dev_env" {
-  source = "../"
-  stage  = "dev"
+resource "aws_s3_bucket" "state" {
+  bucket = "${var.project_name}-states"
+  acl    = "private"
 }
