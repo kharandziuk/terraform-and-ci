465932d92ac2a1f186005bf5d6eeda0172c577ed
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-11-12 12:47:36 +0100

    rename aws_* to * variables

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {
+variable "access_key" {
   description = "Access key to your AWS account "
 }
 
-variable "aws_secret_key" {
+variable "secret_key" {
   description = "Secret key to your AWS account "
 }
 
@@ -12,7 +12,9 @@ variable "aws_region" {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 module "env" {
