8812de436e37679b4bec055dc8979be5c6e1c836
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-12-09 14:53:20 +0100

    add invoke to workflow

--- tasks.py
+++ tasks.py
@@ -5,7 +5,24 @@ import os
 load_dotenv()
 
 CUR_DIR = Path(".")
-ENV_DIR = CUR_DIR / "environments" / "dev"
+
+# return the name of environent depending on the ENV vars
+def _which_environment():
+    if os.getenv("DEPLOYMENT_ENV"):
+        return os.getenv("DEPLOYMENT_ENV")
+    if os.getenv("GITHUB_EVENT_NAME") == "push":
+        return 'staging'
+    return "dev"
+
+ENV_DIR = CUR_DIR / "environments" / _which_environment()
+
+inventory_path = (CUR_DIR / 'inventory.yml').absolute()
+
+print(ENV_DIR)
+
+@task
+def which_environment(c):
+    c.run(f"echo '{_which_environment()}'")
 
 @task
 def apply_infrastructure(c):
@@ -13,12 +30,17 @@ def apply_infrastructure(c):
 
 @task(apply_infrastructure)
 def output_inventory(c):
-    c.run(f"cd {ENV_DIR} && terraform output inventory > {(CUR_DIR / 'inventory.yml' )}")
+    c.run(f"cd {ENV_DIR} && terraform output inventory > {inventory_path}")
 
 @task(output_inventory)
 def provision(c):
-     c.run('ansible-playbook -i inventory.yml playbook.yml')
+     c.run(f'ansible-playbook -i {inventory_path} playbook.yml')
 
 @task
 def destroy_infrastructure(c):
     c.run(f"cd {ENV_DIR} && terraform init --reconfigure && terraform destroy --auto-approve")
+
+@task(provision)
+def all(c):
+    pass
+
