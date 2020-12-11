176c07abc3589c3871a40b22f4898c823e4c0600
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-12-11 13:04:00 +0100

    update tf version and invoke tasks

--- tasks.py
+++ tasks.py
@@ -25,23 +25,27 @@ def which_environment(c):
     c.run(f"echo '{_which_environment()}'")
 
 @task
-def apply_infrastructure(c):
-    c.run(f"cd {ENV_DIR} && terraform init --reconfigure && terraform apply --auto-approve")
+def apply(c):
+    c.run(
+        f"terraform -chdir={ENV_DIR} init --reconfigure && terraform -chdir={ENV_DIR} apply --auto-approve"
+    )
 
 @task(apply_infrastructure)
 def output_inventory(c):
-    c.run(f"cd {ENV_DIR} && terraform output inventory > {inventory_path}")
-    c.run(f"cat {inventory_path}")
+    c.run(
+        f"terraform -chdir={ENV_DIR} output -json inventory | jq -r . > {inventory_path}"
+    )
 
 @task(output_inventory)
 def provision(c):
-     c.run(f'ansible-playbook -i inventory.yml playbook.yml')
+    c.run(f"ansible-playbook -i inventory.yml playbook.yml")
 
 @task
-def destroy_infrastructure(c):
-    c.run(f"cd {ENV_DIR} && terraform init --reconfigure && terraform destroy --auto-approve")
+def destroy(c):
+    c.run(
+        f"terraform -chdir={ENV_DIR} init --reconfigure && terraform -chdir={ENV_DIR} destroy --auto-approve"
+    )
 
 @task(provision)
 def all(c):
     pass
-
